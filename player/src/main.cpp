d3d071c452a86d1f03a5ace688da508c6f79506c
urshofer <u.hofer@tr51.org>
2014-12-12 12:37:50 +0100

    #elif instead of #elseif

--- player/src/main.cpp
+++ player/src/main.cpp
@@ -6,13 +6,13 @@
 
 //========================================================================
 int main( ){
-	#ifdef TARGET_LINUX
+    #ifdef TARGET_LINUX
 		ofAppGlutWindow window;
 		ofSetupOpenGL(&window, 640, 360, OF_WINDOW);
-    #elseif TARGET_OF_IPHONE
+    #elif TARGET_OF_IPHONE
         ofSetupOpenGL(1024,768,OF_FULLSCREEN);
     #else
         ofSetupOpenGL(640, 360, OF_WINDOW);
-	#endif
+    #endif
 	ofRunApp( new ofApp());
 }
